cea5260ec3944c621228bec3a812f8d09429ba7f
Sava Mikalacki <mikalackis@gmail.com>
2024-04-10 14:46:22 +0200

    App update

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_MODULE := ArielGuardian
+LOCAL_MODULE := ArielParental
 
 LOCAL_CERTIFICATE := platform
 
@@ -14,10 +14,10 @@ LOCAL_SYSTEM_EXT_MODULE := true
 
 LOCAL_MODULE_CLASS := APPS
 ifneq (,$(filter eng,$(TARGET_BUILD_VARIANT)))
-  # eng builds use debug ArielGuardian app
+  # eng builds use debug ArielParental app
   LOCAL_SRC_FILES := app-debug.apk
 else
-  # userdebug builds use release ArielGuardian app
+  # userdebug builds use release ArielParental app
   LOCAL_SRC_FILES := app-release.apk
 endif
 
